44f47ad81053257bbdc51e6c9046731bd73e7598
hongri1995930 <549476964@qq.com>
2016-04-22 20:45:48 +0800

    hide the mysql account
    hide the mysql account

--- php/conn.php
+++ php/conn.php
@@ -1,6 +1,6 @@
 <?php
 	header("Content-type:text/html; charset=utf-8");
-        $conn=mysql_connect("127.0.0.1","root","apple");
+        $conn=mysql_connect("127.0.0.1","******","******");
         mysql_query("set names 'UTF_8'");
         mysql_select_db("international",$conn);
 ?> 
